add08ee27a83ad020f0fbfb6df6e0dabf435f4f2
gavinsf <gsfoster031@gmail.com>
2026-04-24 15:09:54 -0700

    Fixes syntax and moves context run to thread

--- include/Node.h
+++ include/Node.h
@@ -6,21 +6,23 @@
 #include <asio.hpp>
 #include <memory>
 #include "TcpServer.h"
+#include <thread>
 
 using asio::ip::tcp;
 
 class node
 {
     private:
+    asio::io_context context_;
+    tcp::resolver resolver_;
+    std::unique_ptr<tcp_server> p_server;
     size_t id;
     size_t port;
-    asio::io_context context;
     std::unordered_map<size_t, std::pair<size_t, size_t>> membership;
     size_t n;
     size_t r_quorum;
     size_t w_quorum;
-    std::unique_ptr<tcp_server> p_server;
-
+    std::thread worker_thread;
 
     public:
     node(size_t id, size_t n, size_t r_quorum, size_t w_quorum);
--- src/Node.cpp
+++ src/Node.cpp
@@ -1,6 +1,7 @@
 #include "Node.h"
 #include <asio.hpp>
 #include "TcpServer.h"
+#include <thread>
 
 using asio::ip::tcp;
 
@@ -10,38 +11,43 @@ node::node(size_t id, size_t n, size_t r_quorum, size_t w_quorum)
       n(n),
       r_quorum(r_quorum),
       w_quorum(w_quorum),
-      context()
+      context_(),
+      resolver_(context_)
 {
     port = -1;
-    auto work = asio::make_work_guard(context);
 }
 
 node::~node()
 {
-    context.stop();
+    context_.stop();
+    if (worker_thread.joinable()) {
+        worker_thread.join();
+    }
 }
 
 void node::start()
 {
     // Check if there is a node on 3000
-    // 
     try 
     {
-        p_server = std::make_unique<tcp_server>(context, 3000);
+        p_server = std::make_unique<tcp_server>(context_, 3000);
         port = 3000;
-
     }
     catch (const std::system_error& e)
     {
         if (e.code() == std::errc::address_in_use)
         {
-            p_server = std::make_unique<tcp_server>(context, 0);
+            p_server = std::make_unique<tcp_server>(context_, 0);
             if (p_server)
+            {
                 port = p_server->get_port();
+            }
         }
         
     }
-    context.run();
+    worker_thread = std::thread([this]() {
+        context_.run(); 
+    });
 }
 
 size_t node::get_port()
